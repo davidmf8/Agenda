d3c4b5ea4cbcf4fc68a8bdda02fe141e02682e17
David Moreno Ferrera <daviduca8@gmail.com>
2013-10-29 19:36:14 +0100

    Escritura foto

--- AgendaUCA/src/com/example/agendauca/Camara.java
+++ AgendaUCA/src/com/example/agendauca/Camara.java
@@ -19,7 +19,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Camara extends Activity{
-	private static final int CAPTURE_IMAGE_ACTIVITY_REQ = 0;
+	private static final int IMAGEN_CAPTURADA = 1;
 		
 	Uri fileUri = null;
 	ImageView fotoImagen = null;
@@ -34,62 +34,44 @@ public class Camara extends Activity{
 		//Idicamos al intent de captura donde se guardará la foto en caso de confirmarlo
 		camara.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 		//Espera el resultado de capturar la imagen
-		startActivityForResult(camara, CAPTURE_IMAGE_ACTIVITY_REQ );
+		startActivityForResult(camara, IMAGEN_CAPTURADA );
 	}
 	
 	//Sobreescribimos este metodo para lo que necesitamos
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		  if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQ) {
+		   Intent cambio_actividad = new Intent();
+		  if (requestCode == IMAGEN_CAPTURADA) {
 		    if (resultCode == RESULT_OK) {
-		      Uri fotoUri = null;
-		      if (data == null) {
-		        // A known bug here! The image should have saved in fileUri
-		        Toast.makeText(this, "Image saved successfully", 
+		        Toast.makeText(this, "Foto guardada con éxito", 
 		                       Toast.LENGTH_LONG).show();
-		        fotoUri = fileUri;
-		      } else {
-		        fotoUri = data.getData();
-		        Toast.makeText(this, "Image saved successfully in: " + data.getData(), 
-		                       Toast.LENGTH_LONG).show();
-		      }
-		      mostrarFoto(fotoUri.getPath());
-		    } else if (resultCode == RESULT_CANCELED) {
-		      Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
-		      Intent cambio_actividad = new Intent();
+		        cambio_actividad.setClass(this, Camara.class);
+			    startActivity(cambio_actividad);
+		    } 
+		    else if (resultCode == RESULT_CANCELED) {
+		      Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
 		      cambio_actividad.setClass(this, MainActivity.class);
-			  startActivity(cambio_actividad);
-		    } else {
-		      Toast.makeText(this, "Callout for image capture failed!", 
-		                     Toast.LENGTH_LONG).show();
-		    }
+		      startActivity(cambio_actividad);
+		    } 
 		  }
+		  
+		  
+	      
 	}
 	
 	private File ficheroFoto() {
-		
-		  File directory = new File(Environment.getExternalStoragePublicDirectory(
-		                Environment.DIRECTORY_PICTURES), getPackageName());
-		  
-		  if (!directory.exists()) {
-		    if (!directory.mkdirs()) {
-		      return null;
-		    }
+		  //Creamos directorio foto
+		  File directorioFoto = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "Fotos");
+
+		  //SI este directorio no estáen memoria externa, lo creamos
+		  if (!directorioFoto.exists()) {
+		     directorioFoto.mkdir();
 		  }
 		  
-		  String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.US).format(new Date());
+		  String horaLocal = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.ROOT).format(new Date());
 		  
-		  return new File(directory.getPath() + File.separator + "IMG_"  
-		                    + timeStamp + ".jpg");
+		  return new File(directorioFoto.getPath() + File.separator + "IMG_"  
+		                    + horaLocal + ".jpg");
 	}
-	
-	private void mostrarFoto(String fotoUri) {
-		  File imageFile = new File (fotoUri);
-		  if (imageFile.exists()){
-		     Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-		     BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
-		     fotoImagen.setScaleType(ImageView.ScaleType.FIT_CENTER);
-		     fotoImagen.setImageDrawable(drawable);
-		  }       
-		  
-	}
+
 }
