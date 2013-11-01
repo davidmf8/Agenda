816868bb025f9dc66a636967dd870f8cc3d1d571
David Moreno Ferrera <daviduca8@gmail.com>
2013-11-01 13:05:52 +0100

    Guarda en memoria interna cuando debe guardar en externa.

--- AgendaUCA/src/com/example/agendauca/Camara.java
+++ AgendaUCA/src/com/example/agendauca/Camara.java
@@ -7,10 +7,8 @@ import java.util.Date;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -29,11 +27,9 @@ public class Camara extends Activity{
 		super.onCreate(savedInstanceState);
 		//Intent camara. Se le indica que la accion del intent será capturar una imagen
 		Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		//Devuleve el fichero donde se guardará
 		fileUri = Uri.fromFile(ficheroFoto());
 		//Idicamos al intent de captura donde se guardará la foto en caso de confirmarlo
 		camara.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-		//Espera el resultado de capturar la imagen
 		startActivityForResult(camara, IMAGEN_CAPTURADA );
 	}
 	
@@ -60,18 +56,14 @@ public class Camara extends Activity{
 	
 	private File ficheroFoto() {
 		  //Creamos directorio foto
-		  File directorioFoto = new File(Environment.getExternalStoragePublicDirectory(
-	              Environment.DIRECTORY_PICTURES), "Fotos");
-
-		  //SI este directorio no estáen memoria externa, lo creamos
-		  if (!directorioFoto.exists()) {
-		     directorioFoto.mkdir();
-		  }
-		  
-		  String horaLocal = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.ROOT).format(new Date());
-		  
-		  return new File(directorioFoto.getPath() + File.separator + "IMG_"  
-		                    + horaLocal + ".jpg");
+		File dir = new File(this.getExternalFilesDir(Environment.DIRECTORY_DCIM),  "FotosAgenda");
+		if(!dir.exists()){
+			dir.mkdir();
+		}
+		
+        String horaLocal = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.ROOT).format(new Date());
+        File foto = new File(dir, "IMG_" + horaLocal + ".jpg");
+		return foto;
 	}
 
 }
