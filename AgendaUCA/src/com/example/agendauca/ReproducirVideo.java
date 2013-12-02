e75c4df0125ff0fad0196bce6ec56ae4c04c4611
David Moreno Ferrera <daviduca8@gmail.com>
2013-12-02 16:47:30 +0100

    Video funcionando

---
+++ AgendaUCA/src/com/example/agendauca/ReproducirVideo.java
@@ -0,0 +1,32 @@
+package com.example.agendauca;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class ReproducirVideo extends Activity{
+
+	VideoView miVideo;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_reproduccion_video);
+		
+		Bundle datosIntent = this.getIntent().getExtras();
+		String rutaVideo = datosIntent.getString("Video");
+		
+		 miVideo = (VideoView) findViewById(R.id.ReproductorVideo);
+         Uri ruta = Uri.parse(rutaVideo);
+
+		 MediaController reproductor = new MediaController(this);    
+		 miVideo.setMediaController(reproductor);
+		 
+		 miVideo.setVideoURI(ruta);
+		 miVideo.start();
+		
+		
+	}
+}
